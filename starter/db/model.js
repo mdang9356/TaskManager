efca767528b86c191b1cfea72b40002ad96c56f4
mdang9356@my.sullivan.edu <mdang9356@my.sullivan.edu>
2021-11-21 23:51:20 -0500

    Adding CRUD actions to back end

--- starter/db/model.js
+++ starter/db/model.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    complete: Boolean
+    name: {
+        type: String,
+        required: [true, 'Task must have name'],
+        trim: true,
+        maxLength: [50, 'Task name is no more than 50 character']
+    },
+    complete:{
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports = mongoose.model('Task',TaskSchema);
